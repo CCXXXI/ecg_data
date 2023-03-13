f9856a034dc96160e09d0ffe49e66dd98c598550
CCXXXI <ccxxxi@qq.com>
2023-03-14 03:37:40 +0800

    test: add tests

---
+++ tests/test_main.py
@@ -0,0 +1,37 @@
+import numpy as np
+from pytest import fixture
+from main import output_len_s, output_fs, get_record, record_to_array
+
+
+@fixture
+def record():
+    return get_record()
+
+
+@fixture
+def array(record):
+    return record_to_array(record)
+
+
+@fixture
+def saved_array():
+    return np.column_stack(
+        (
+            np.loadtxt("../assets/lead I.txt"),
+            np.loadtxt("../assets/lead II.txt"),
+        )
+    )
+
+
+def test_get_record(record):
+    assert record.fs >= output_fs
+    assert record.sig_len // record.fs >= output_len_s
+    assert record.sig_name == ["I", "II"]
+
+
+def test_record_to_array(array):
+    assert array.shape == (output_len_s * output_fs, 2)
+
+
+def test_saved_array(saved_array, array):
+    assert np.allclose(saved_array, array)
